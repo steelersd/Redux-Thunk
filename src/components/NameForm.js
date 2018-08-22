0e1f35f277cc3a2fcd33f82edb2ad2a65e8bebd4
Adam <steelersd@users.noreply.github.com>
2018-08-22 07:38:48 -0500

    Code cleanup
    Cleaned up some styled components

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -5,41 +5,6 @@ import Row from "../styles/Row";
 import Section from "../styles/Section";
 import PropTypes from "prop-types";
 
-const StyledForm = styled.form`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  input {border: 1px solid #555};
-  textarea {border: 1px solid #555};
-`;
-
-const Label = styled.label`
-  display:inline-block;
-  text-align: left;
-  width: 140px;
-  font-family: 'Roboto', sans-serif;
-  font-size: 1.1em;
-  font-weight: 400;
-`;
-
-const input = css`  padding: 5px;
-  margin: 8px 3px;
-  width: 250px;
-  box-sizing: border-box;
-  outline: none;
-  font-family: 'Roboto', sans-serif;
-  font-size: 1.1em;
-  font-weight: 300;
-`;
-
-const Input = styled.input`
-  ${input}
-`;
-const TextArea = styled.textarea`
-  ${input}
-`;
-
 const NameForm = props => {
   const rand = val => {
     return new Date().valueOf() + val;
